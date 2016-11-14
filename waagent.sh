9b0990cc01c7aa22ff8a078b1ee371f49ab385c0
tamcclur <tamcclur@microsoft.com>
2016-11-14 13:10:04 -0800

    changing pull method to pull from latest release tarball, rather than directly from master branch

--- waagent.sh
+++ waagent.sh
@@ -9,21 +9,20 @@ if ! type "pip" > /dev/null; then
   echo "pip not installed, please install python-pip"
 fi
 
-if ! type "git" > /dev/null; then
-  echo "giit not installed, please install git"
-fi
-
 pip list | grep setuptools > /dev/null 2>&1
 if [ $? -eq 1 ]; then
   echo "Python setuptools not found, please install setuptools"
   echo "pip install setuptools"
 fi
 
-echo "Downloading waagent from github."
-git clone https://github.com/Azure/WALinuxAgent.git /tmp/WALinuxAgent
+mkdir -p /tmp/WALinuxAgent
 
+# Installing latest release from the github API
+echo "Downloading waagent from github."
+wget -O /tmp/WALinuxAgent.tar.gz `curl -s https://api.github.com/repos/Azure/WALinuxAgent/releases | grep tarball_url | head -n 1 | cut -d '"' -f 4`
+tar xzf /tmp/WALinuxAgent.tar.gz -C /tmp/WALinuxAgent
 echo "Installing waagent"
-python /tmp/WALinuxAgent/setup.py install --force
+python /tmp/WALinuxAgent/`ls /tmp/WALinuxAgent`/setup.py install --force
 sleep 0.5
 
 echo "Installation done, restarting waagent."
@@ -33,9 +32,11 @@ if ! type "systemctl"; then
 else
   systemctl daemon-reload && systemctl restart waagent
   sleep 0.5
-  exit 0
 fi
-  
+
+# Cleanup
+rm -rf /tmp/WALinuxAgent /tmp/WALinuxAgent.tar.gz
+
 # Check if running
 waagent --version
 if [ $? -eq 1 ]; then
